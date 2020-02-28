8f47c7806e289c74e3cb658d9533f049e5e2f54d
Zac Foster <43626969+thezacfoster@users.noreply.github.com>
2020-02-28 16:08:47 -0500

    added Ruins.cpp (no definitions yet)

---
+++ Ruins.cpp
@@ -0,0 +1,35 @@
+/* Project: CS395 Alpha Milestone
+-- Project Description: An experimental experience manager for the Camelot Virtual Environment.
+-- Authors: Jake Hayden, John Colfer, Mac McNerney, Zac Foster
+-- File: Ruins.cpp
+-- File Description:
+--		A file that defines a class representing the ruins location in the game. Coincides with the
+--		"Ruins" Place in Camelot, and contains data and code for what can occur in this location.
+*/
+#include "Ruins.h"
+
+Ruins::Ruins() {
+	
+}
+
+//overloaded constructor for giving the location a Camelot name immediately
+Ruins::Ruins(string name) {
+	
+}
+
+//functions for enabling/disabling furniture
+void Ruins::enableDirtPile(bool enabled) {
+
+}
+void Ruins::enablePlant(bool enabled) {
+
+}
+void Ruins::enableAltar(bool enabled) {
+
+}
+void Ruins::enableThrone(bool enabled) {
+
+}
+void Ruins::enableChest(bool enabled) {
+
+}
