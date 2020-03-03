84ab70ce9b7b8c06eacb7d7e215367940628ec79
JHaydenUKY <57688785+JHaydenUKY@users.noreply.github.com>
2020-03-02 19:47:03 -0500

    Added potion to alter for flashback transition

--- Chapter2.cpp
+++ Chapter2.cpp
@@ -5,8 +5,6 @@
 -- File Description: The execution related to Chapter 2.
 */
 #include "Chapter2.h"
-#include <chrono>
-#include <thread>
 //#include "functions.cpp"
 #include <chrono>
 #include <thread>
@@ -55,7 +53,7 @@ void Chapter2::flashback1() {
 	function.Action("SetCameraMode(Follow)", true);
 	function.Action("Die(Arlan)", true);
 	function.Action("FadeOut()", true);
-
+	
 	//
 }
 
@@ -64,64 +62,20 @@ bool Chapter2::setupCurrentRuins(string name) {
 	currentRuins = Ruins(name);
 
 	//character setup
-	function.SetupCharacter("Arlan", "B", "Peasant", "Spiky", "Brown", "CurrentRuins.Exit");
+	function.SetupCharacter("Arlan", "B", "LightArmour", "Long", "Brown", "CurrentRuins.Exit");
+
+	//items and their placement
+	function.Action("CreateItem(LeaderFlashPotion, GreenPotion)", true);//potion to activate flashback 1
+	function.Action("SetPosition(LeaderFlashPotion, CurrentRuins.Altar)", true);
 
 	//icons
-	currentRuins.icons.push_back(Icon("Examine", "Hand", "CurrentRuins.Altar", "Examine the Altar", "true"));
+	currentRuins.icons.push_back(Icon("Take", "Hand", "LeaderFlashPotion", "Take the Potion", "true"));
 	function.SetupIcons(currentRuins.icons);
 
 	function.Action("ShowMenu()", true);
 
 	return true;
 }
-
-bool Chapter2::setupCottage(string name) {
-	currentCottage = Cottage(name);
-
-	function.Action("CreateItem(Letter, OpenScroll)", true);
-	function.Action("SetPosition(Letter, CurrentCottage.Table)", true);
-
-	//character setup
-	function.SetupCharacter("Mathias", "F", "LightArmour", "Long", "Brown", "CurrentCottage.Bed");
-
-	//icons
-	currentCottage.icons.push_back(Icon("Open", "Exit", "CurrentCottage.Door", "Leave the Room", "true"));
-	currentCottage.icons.push_back(Icon("Read", "Research", "Letter", "Read the Letter", "true"));
-	function.SetupIcons(currentCottage.icons);
-
-	function.Action("ShowMenu()", true);
-
-	return true;
-}
-
-bool Chapter2::setupForestPath(string name) {
-	currentForestPath = ForestPath(name);
-
-	//character setup
-	function.SetupCharacter("Mathias", "F", "LightArmour", "Long", "Brown", "CurrentForestPath.EastEnd");
-
-	//icons
-
-	function.Action("ShowMenu()", true);
-
-	return true;
-}
-
-bool Chapter2::setupCity(string name) {
-	currentCity = City(name);
-
-	//character setup
-	function.SetupCharacter("Mathias", "F", "LightArmour", "Long", "Brown", "CurrentCity.WestEnd");
-
-	//icons
-
-	function.Action("ShowMenu()", true);
-
-	return true;
-}
-
-
-
 
 // location execution functions.
 void Chapter2::runCurrentRuins() {
@@ -132,7 +86,6 @@ void Chapter2::runCurrentRuins() {
 		//Gets the first word that isn't "input"
 		modified_I = function.splitInput(i, 6, false);
 
-		//If it's under the "Selected" keyword
 		bool inputWasCommon = function.checkCommonKeywords(i, modified_I, "Arlan", arlanInv);
 
 		if (!inputWasCommon) {
@@ -142,68 +95,10 @@ void Chapter2::runCurrentRuins() {
 			}
 			//if the player is in the CurrentRuins
 			else if (inCurrentRuins) {
-				if (modified_I == "Examine") {
+				if (modified_I == "Take") {
+					function.Action("Take(Arlan, LeaderFlashPotion)", true);
 					flashback1();
 				}
-			}
-		}
-	}
-}
-
-void Chapter2::runCottage() {
-	while (true) {
-		string i;
-		getline(cin, i);
-
-		//Gets the first word that isn't "input"
-		modified_I = function.splitInput(i, 6, false);
-
-		bool inputWasCommon = function.checkCommonKeywords(i, modified_I, "Mathias", mathiasInv);
-
-		if (!inputWasCommon) {
-
-			//If it's under the "Talk" keyword
-			if (modified_I == "Talk") {
-				modified_I = function.splitInput(i, 0, true);
-			}
-		}
-		
-	}
-}
-
-void Chapter2::runForestPath() {
-	while (true) {
-		string i;
-		getline(cin, i);
-
-		//Gets the first word that isn't "input"
-		modified_I = function.splitInput(i, 6, false);
-
-		bool inputWasCommon = function.checkCommonKeywords(i, modified_I, "Mathias", mathiasInv);
-
-		if (!inputWasCommon) {
-			//If it's under the "Talk" keyword
-			if (modified_I == "Talk") {
-				modified_I = function.splitInput(i, 0, true);
-			}
-		}
-	}
-}
-
-void Chapter2::runCity() {
-	while (true) {
-		string i;
-		getline(cin, i);
-
-		//Gets the first word that isn't "input"
-		modified_I = function.splitInput(i, 6, false);
-
-		bool inputWasCommon = function.checkCommonKeywords(i, modified_I, "Mathias", mathiasInv);
-
-		if (!inputWasCommon) {
-			//If it's under the "Talk" keyword
-			if (modified_I == "Talk") {
-				modified_I = function.splitInput(i, 0, true);
 			}
 		}
 	}
